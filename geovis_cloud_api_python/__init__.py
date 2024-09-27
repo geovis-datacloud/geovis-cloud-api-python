f7f62f1c265ca538c92334174fe5909bacf91fab
qiaoyk <qiaoyk@geovis.com.cn>
2024-09-27 11:16:54 +0800

    first commit

---
+++ geovis_cloud_api_python/__init__.py
@@ -0,0 +1,1 @@
+from .sign import (CloudApiSign, PaymentSign, CertificationSign, DataCloudSign)
---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="geovis-cloud-api-python",
+    version="0.0.1",
+    keywords=("pip", "tool", "sign"),
+    description="geovis cloud api",
+    license="MIT Licence",
+
+    url="https://github.com/geovis-datacloud/geovis-cloud-api-python.git",
+    author="qiao",
+    author_email="979146919@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any"
+)
